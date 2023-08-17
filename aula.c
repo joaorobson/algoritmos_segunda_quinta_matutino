563fdbea9029d3b5a913d3a1f08931fcc4a7f00f
joaorobson <joaorobsonmartins@gmail.com>
2023-08-17 09:31:46 -0300

    removendo printf desnecessario

--- aula.c
+++ aula.c
@@ -6,8 +6,7 @@ int main(){
 
     scanf("%c", &turno);
 
-    printf("Digite o turno do aluno:\n");
-    printf("Digite as notas dos alunos:\n");
+    printf("Digite as notas dos alunos abaixo:\n");
     scanf("%f", &nota1);
     scanf("%f", &nota2);
     scanf("%f", &nota3);
